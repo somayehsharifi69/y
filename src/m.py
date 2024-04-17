b9cb82620ff04cc7cf19f442477796e02d2096b3
somayeh2022 <somayeh.sharifi69@yahoo.com>
2024-04-17 12:23:54 +0200

    2

--- src/m.py
+++ src/m.py
@@ -1,7 +1,8 @@
-def f(a, b):
+def f(a: int, b: int) -> int:
+    """Function to add two integers."""
     return a + b
 
 if __name__ == '__main__':
-    a = 2
-    b = 3
+    a: int = 2
+    b: int = 3
     print(f(a, b))
